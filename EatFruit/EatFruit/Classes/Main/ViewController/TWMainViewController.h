2cdf70985be4d2a139b09984c5f0c7df2b44e58f
tianwei <tianwei223344@163.com>
2017-08-11 09:58:51 +0800

    初始化项目

---
+++ EatFruit/EatFruit/Classes/Main/ViewController/TWMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWMainViewController.h
+//  EatFruit
+//
+//  Created by 田伟 on 2017/8/11.
+//  Copyright © 2017年 田伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWMainViewController : UIViewController
+
+@end
---
+++ EatFruit/EatFruit/Classes/Main/ViewController/TWMainViewController.m
@@ -0,0 +1,32 @@
+//
+//  TWMainViewController.m
+//  EatFruit
+//
+//  Created by 田伟 on 2017/8/11.
+//  Copyright © 2017年 田伟. All rights reserved.
+//
+
+#import "TWMainViewController.h"
+
+@interface TWMainViewController ()
+
+@end
+
+@implementation TWMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = TWRandomColor;
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+@end
