ca272f86987a866ff069903f6cbc693461320aaa
tianwei <tianwei223344@163.com>
2017-08-17 09:25:21 +0800

    修改小bug

--- EatFruit/EatFruit/Classes/Home/View/BabyImageView.m
+++ EatFruit/EatFruit/Classes/Home/View/BabyImageView.m
@@ -25,30 +25,29 @@
 // 检查是否抓住
 - (BOOL)checkIfCaught:(CGRect)frame{
     // 碰撞检测（交集）
-    CGRect candyFrame = frame;
-
+//    CGRect candyFrame = frame;
     // 判断两个结构体是否有交错
-    bool oneRect = CGRectIntersectsRect(candyFrame, self.frame);
-    
-//    if (oneRect) {
-//        NSLog(@"You did it!");
-//        return YES;
-//    }
-//    NSLog(@"Fail");
-//    return NO;
-    return oneRect;
+//    bool oneRect = CGRectIntersectsRect(candyFrame, self.frame);
+//    
+////    if (oneRect) {
+////        NSLog(@"You did it!");
+////        return YES;
+////    }
+////    NSLog(@"Fail");
+////    return NO;
+//    return oneRect;
     
     // 顶部接受
-//    int candy1x = frame.origin.x;
-//    int candy2x = frame.origin.x + frame.size.width;
-//    
-//    int netX1x = self.frame.origin.x;
-//    int netX2x = self.frame.origin.x + self.frame.size.width;
-//    
-//    if( 5 + candy1x >= netX1x && candy2x <= netX2x + 5){
-//        return YES;
-//    }
-//    return NO;
+    int candy1x = frame.origin.x;
+    int candy2x = frame.origin.x + frame.size.width;
+    
+    int netX1x = self.frame.origin.x;
+    int netX2x = self.frame.origin.x + self.frame.size.width;
+    
+    if( 5 + candy1x >= netX1x && candy2x <= netX2x + 5){
+        return YES;
+    }
+    return NO;
 }
 
 @end
